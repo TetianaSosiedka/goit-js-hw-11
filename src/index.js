6b8a8779db2ab6efce54231f356b43e1f4405f59
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-31 15:38:05 +0300

    marcap article

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
 import Notiflix from 'notiflix';
 
 import ApiServices from './script/api-services.js';
+import RenderList from './script/render-list';
 
 const refs = {
   form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
 const newApiServices = new ApiServices();
+const newRenderList = new RenderList(refs.gallery);
 
 refs.form.addEventListener('submit', onSearch);
+
 function onSearch(event) {
   event.preventDefault();
   const searchQuery = event.currentTarget.elements.searchQuery.value.trim('');
   if (searchQuery != '') {
     newApiServices.query = searchQuery;
     newApiServices.searchImg().then(hits => {
-      if (hits) {
-        hits.map(
-          ({
-            webformatURL,
-            largeImageURL,
-            tags,
-            likes,
-            views,
-            comments,
-            downloads,
-          }) => {
-            console.log({
-              webformatURL,
-              largeImageURL,
-              tags,
-              likes,
-              views,
-              comments,
-              downloads,
-            });
-          }
+      if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
         );
+
+        return;
       }
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
+      newRenderList.params = hits;
+      newRenderList.marcapGallery();
     });
+  } else {
+    Notiflix.Notify.info('Please enter a request.');
   }
 }
---
+++ src/script/render-list.js
@@ -0,0 +1,49 @@
+export default class RenderList {
+  constructor(refsGallery) {
+    this.paramsArticle = [];
+    this.refsGallery = refsGallery;
+  }
+  marcapGallery() {
+    const list = this.renderList();
+    this.refsGallery.insertAdjacentHTML('beforeend', list);
+  }
+  renderList() {
+    return this.paramsArticle
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>${downloads}</b>
+    </p>
+  </div>
+</div>`;
+        }
+      )
+      .join('');
+  }
+  get params() {
+    return this.paramsArticle;
+  }
+  set params(newParams) {
+    this.paramsArticle = newParams;
+  }
+}
