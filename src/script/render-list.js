57c89a7154bdd8da729e5e446e60e4c73db732bd
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-06-02 19:05:41 +0300

    added style

--- src/script/render-list.js
+++ src/script/render-list.js
@@ -24,7 +24,6 @@ export default class RenderList {
   <img class='gallery__image' src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-<<<<<<< Updated upstream
       <b>Likes</b> ${likes}
     </p>
     <p class="info-item">
@@ -36,14 +35,8 @@ export default class RenderList {
     <p class="info-item">
       <b>Downloads</b> ${downloads}
     </p>
-=======
-      <b>Likes</b>${likes}</p>
-        <p class="info-item"><b>Views</b>${views}</p>
-        <p class="info-item"><b>Comments</b>${comments}</p>
-        <p class="info-item"><b>Downloads</b>${downloads}</p>
->>>>>>> Stashed changes
-  </div>
-  </a>`;
+  </div></a>
+`;
         }
       )
       .join('');
